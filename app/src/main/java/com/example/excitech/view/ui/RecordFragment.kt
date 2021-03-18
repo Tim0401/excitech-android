fdf18d8397e45dd4e7c7fdbe778ddb6208f54474
Tsukasa Murata <stepfootprint@gmail.com>
2021-03-19 05:06:10 +0900

    音声録音実装

--- app/src/main/java/com/example/excitech/view/ui/RecordFragment.kt
+++ app/src/main/java/com/example/excitech/view/ui/RecordFragment.kt
@@ -1,16 +1,25 @@
 package com.example.excitech.view.ui
 
-import androidx.lifecycle.ViewModelProvider
+import android.Manifest
+import android.R.attr.button
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.example.excitech.R
 import com.example.excitech.viewModel.RecordViewModel
+import permissions.dispatcher.NeedsPermission
+import permissions.dispatcher.RuntimePermissions
+
 
 const val TAG_OF_RECORD_FRAGMENT = "RecordFragment"
 
+@RuntimePermissions
 class RecordFragment : Fragment() {
 
     companion object {
@@ -18,18 +27,53 @@ class RecordFragment : Fragment() {
     }
 
     private lateinit var viewModel: RecordViewModel
+    private lateinit var recordButton: ImageButton
+    private lateinit var recordingDuration: TextView
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.record_fragment, container, false)
+        val view = inflater.inflate(R.layout.record_fragment, container, false)
+        recordButton = view.findViewById(R.id.recordButton)
+        recordingDuration = view.findViewById(R.id.time)
+
+        // ボタンで録音開始
+        recordButton.setOnClickListener{
+            this.recordSoundWithPermissionCheck()
+        }
+        return view
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(RecordViewModel::class.java)
+        viewModel = ViewModelProvider(this, RecordViewModel.Factory(
+            requireActivity().application
+        )).get(RecordViewModel::class.java)
         // TODO: Use the ViewModel
+        viewModel.isRecordingLiveData.observe(viewLifecycleOwner, Observer { isRecording ->
+            isRecording?.let {
+                if (it) {
+                    recordButton.setImageResource(R.drawable.stop)
+                } else {
+                    recordButton.setImageResource(R.drawable.record)
+                }
+            }
+        })
+
+        viewModel.recordingDurationLiveData.observe(viewLifecycleOwner, Observer { duration ->
+            duration?.let {
+                val hour = duration / (60 * 60)
+                val minute = duration / 60
+                val second = duration % 60
+                recordingDuration.text = "%02d:%02d:%02d".format(hour, minute, second)
+
+            }
+        })
+    }
+    @NeedsPermission(Manifest.permission.RECORD_AUDIO)
+    fun recordSound(){
+        viewModel.onRecord();
     }
 
 }
--- app/src/main/java/com/example/excitech/viewModel/RecordViewModel.kt
+++ app/src/main/java/com/example/excitech/viewModel/RecordViewModel.kt
@@ -1,7 +1,80 @@
 package com.example.excitech.viewModel
 
+import android.R.attr.delay
+import android.annotation.SuppressLint
+import android.app.Application
+import android.media.MediaRecorder
+import android.util.Log
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import java.io.IOException
+import java.util.*
 
-class RecordViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+private const val LOG_TAG = "AudioRecordTest"
+
+class RecordViewModel(application: Application) : AndroidViewModel(application) {
+    var isRecordingLiveData: MutableLiveData<Boolean> = MutableLiveData(false)
+    var recordingDurationLiveData: MutableLiveData<Int> = MutableLiveData(0)
+
+    private val timer = Timer()
+    private var fileName: String = "aaa.wav"
+    private var recorder: MediaRecorder? = null
+    @SuppressLint("StaticFieldLeak")
+    private val context = getApplication<Application>().applicationContext
+
+    fun onRecord() {
+        if(isRecordingLiveData.value == true){
+            isRecordingLiveData.postValue(false)
+            stopRecording()
+        } else {
+            isRecordingLiveData.postValue(true)
+            startRecording()
+        }
+    }
+
+    private fun startRecording() {
+        recorder = MediaRecorder().apply {
+            setAudioSource(MediaRecorder.AudioSource.MIC)
+            setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
+            setOutputFile(context.filesDir.path + '/' +fileName)
+            setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
+
+            Log.d(LOG_TAG,context.filesDir.path + '/' +fileName)
+
+            try {
+                prepare()
+            } catch (e: IOException) {
+                Log.e(LOG_TAG, "prepare() failed")
+            }
+
+            start()
+
+            timer.scheduleAtFixedRate(object : TimerTask() {
+                override fun run() {
+                    recordingDurationLiveData.postValue(recordingDurationLiveData.value!! + 1)
+                }
+            }, 1000, 1000)
+        }
+    }
+
+    private fun stopRecording() {
+        recorder?.apply {
+            stop()
+            reset()
+            release()
+        }
+        timer.cancel()
+        recordingDurationLiveData.postValue(0)
+        recorder = null
+    }
+
+    class Factory(private val application: Application) : ViewModelProvider.NewInstanceFactory() {
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            return RecordViewModel(application) as T
+        }
+    }
+
 }
